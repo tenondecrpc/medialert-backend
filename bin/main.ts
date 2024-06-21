511cb680cfe9a53d02e9229572c386cadfcbcb34
Cristian Paniagua <tenondecrpc@gmail.com>
2024-06-20 22:52:43 -0400

    feat(project): Added DoseAlertQueue

--- bin/main.ts
+++ bin/main.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { MedialertBackendStack } from '../lib/medialert-backend-stack';
+import { MainStack } from '../lib/main-stack';
 
 const app = new cdk.App();
-new MedialertBackendStack(app, 'MedialertBackendStack', {
+new MainStack(app, 'MainStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
---
+++ test/main.test.ts
@@ -0,0 +1,15 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Main from '../lib/main-stack';
+
+test('SQS DoseAlertQueue Created', () => {
+  const app = new cdk.App();
+    // WHEN
+  const stack = new Main.MainStack(app, 'MainStack');
+    // THEN
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    VisibilityTimeout: 300
+  });
+});
