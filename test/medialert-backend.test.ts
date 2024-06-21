5626970cc5b78569832540f01f731f47f83801d1
Cristian Paniagua <tenondecrpc@gmail.com>
2024-06-20 21:54:18 -0400

    feat(project): Initial commit

---
+++ test/medialert-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MedialertBackend from '../lib/medialert-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/medialert-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MedialertBackend.MedialertBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
